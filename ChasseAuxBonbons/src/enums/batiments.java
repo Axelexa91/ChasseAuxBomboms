a3277aa82bcf004b2efeb1111c1827522ead9a1d
Antoine Fleury <antoine.fleury@isen.yncrea.fr>
2018-11-07 21:51:00 +0100

    ajout des enums filles + pieces + batiments

---
+++ ChasseAuxBonbons/src/enums/batiments.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package enums;
+
+/**
+ *
+ * @author Antoine
+ */
+public enum batiments {
+    maison_hantee ("maison_hantee"),
+    eglise ("eglise"),
+    boucherie ("boucherie"),
+    prison ("prison");
+    
+    String lieu;
+    
+    private batiments(String lieu) {
+        this.lieu = lieu;
+    }
+
+    public String getLieu() {
+        return new String(lieu);
+    }
+
+    public void setLieu(String lieu) {
+        this.lieu = lieu;
+    }
+}
