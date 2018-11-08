b02e8e7cc03c293dfc0afad6ca2bdf72e4de8eeb
Axelexa <axel.zerbin@isen.yncrea.fr>
2018-11-08 10:12:35 +0100

    création des classes Lieux et pièces
    Lieux pour les batiments et autres sans bonbons,

---
+++ ChasseAuxBonbons/src/Lieux/Piece/Piece.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lieux.Piece;
+
+import Lieux.Lieu;
+
+/**
+ *
+ * @author axelz
+ */
+public class Piece extends Lieu{
+    
+    public Piece(String Nom,int bonbon) {
+        super(Nom);
+        bonbon = bonbon;
+    }
+    
+    int bonbon;
+
+    public int getBonbon() {
+        return bonbon;
+    }
+
+    public void setBonbon(int bonbon) {
+        this.bonbon = bonbon;
+    }
+    
+    
+}
